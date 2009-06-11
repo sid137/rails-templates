1bd0e60e21446e58c171cb2e162af0589b32ccf0
Sidney Burks <sid137@gmail.com>
2009-06-11 22:04:20 +0200

    added regexp for cucmber path

--- gems.rb
+++ gems.rb
@@ -51,9 +51,35 @@ end
 END
 
 
-file "features/support/local_path.rb", <<-END
-END
+file "features/support/local_paths.rb", <<-END
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in webrat_steps.rb
+  #
+  def path_to(page_name)
+    case page_name
 
+    when /the home\*page/
+      '/'
+
+    # Add more mappings here.
+    # Here is a more fancy example:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
+
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
+    end
+  end
+end
+
+World(NavigationHelpers)
+END
 
 file "app/views/layouts/application.html.haml", <<-END
 !!!
