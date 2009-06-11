9c804da90fc5ff9781b812af3caf11a9b241a0fa
Sidney Burks <sid137@gmail.com>
2009-06-11 22:08:54 +0200

    fixed another # escaping error with template

--- gems.rb
+++ gems.rb
@@ -72,8 +72,8 @@ module NavigationHelpers
     #     user_profile_path(User.find_by_login($1))
 
     else
-      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
+      raise "Can't find mapping from \"\#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in \#{__FILE__}"
     end
   end
 end
