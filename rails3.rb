3147c617230faebed2a938b5e0f0f9e524097ae0
Sidney Burks <sid137@gmail.com>
2010-03-29 02:08:00 +0200

    compat for rails3

--- rails3.rb
+++ rails3.rb
@@ -17,8 +17,6 @@ end
 
 
 # Install haml/sass/compass 
-apply "http://www.compass-style.org/rails/installer"
-
 run "compass --rails -f blueprint . --css-dir=public/stylesheets/ --sass-dir=app/stylesheets"
 
 
