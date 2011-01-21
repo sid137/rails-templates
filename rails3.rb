a96be6a9559dc4291374e5967f0ef0bc5d1067d0
Sidney Burks <sid137@gmail.com>
2011-01-21 11:20:47 +0100

    rails 3

--- rails3.rb
+++ rails3.rb
@@ -13,74 +13,58 @@ run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec tou
 file "Gemfile",<<-END
 # Edit this Gemfile to bundle your application's dependencies.
 source 'http://gemcutter.org'
-source 'http://gems.github.com'
+source 'rubygems.org'
+
 
 ## Bundle edge rails:
-gem "rails", :git => "git://github.com/rails/rails.git"
-gem "sqlite3-ruby"
+#gem "rails", :git => "git://github.com/rails/rails.git"
+gem "sqlite3-ruby", :require => 'sqlite3'
 
-#gem "bson_ext"
-#gem "mongoid", :git => "git://github.com/durran/mongoid.git"
-
-#gem "warden"
-#gem "devise", "1.1.pre4", :git => "git://github.com/plataformatec/devise.git"
-
-
-gem "haml"
-gem "compass"
-#gem "bluecloth"
+gem "haml-rails"
 
 group :test do
-#  gem 'ZenTest'
 #  gem "rspec", :git => "git://github.com/rspec/rspec.git"
 #  gem "rspec-rails", :git => "git://github.com/rspec/rspec-rails.git"
-#  gem 'capybara',         :git => 'git://github.com/jnicklas/capybara.git'
-#  gem 'database_cleaner', :git => 'git://github.com/bmabey/database_cleaner.git'
-#  gem 'cucumber-rails',   :git => 'git://github.com/aslakhellesoy/cucumber-rails.git'
 end
 
 #gem "faker"
 group :development do
 #  gem "looksee", :group => [:development, :test]
 end
-
-#gem "jquery_helpers"
-#gem "newrelic_rpm"
 END
 
 
 file "app/views/layouts/application.html.haml",<<-END
-!!! 5
-%html(xmlns="http://www.w3.org/1999/xhtml")
+!!! 
+%html(lang='en')
   %head
-    %meta(http-equiv="content-type" content="text/html" charset="utf-8")
-    %title Application Template
+    %meta(charset='utf-8')
 
-    = stylesheet_link_tag 'screen.css', :media => 'screen, projection'
-    = stylesheet_link_tag 'print.css', :media => 'print'
-    /[if lt IE 8]
-      = stylesheet_link_tag 'ie.css', :media => 'screen, projection'
+    %title 
 
-    = javascript_include_tag 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'
+    = javascript_include_tag "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"
     = javascript_include_tag 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js'
+    = stylesheet_link_tag "screen", :media => "screen, projection"
+    = stylesheet_link_tag "print", :media => "print"
 
-    = javascript_include_tag 'http://www.google.com/jsapi?key=ABQIAAAAWE_Mi-8Zmi4wixpIeFXIWxSah9S-j4zSd7OuZVJ9P6HK1pErfhRLys8c9D8s0Q_R-Itr4xemDQHIog', :charset => 'utf-8'
+    <!--[if IE]><link href="stylesheets/ie.css" rel="stylesheet" /><![endif]-->
+    <!-- HTML5 Shiv http://code.google.com/p/html5shiv/ -->
+    <!--[if lt IE 9]> 
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> 
+    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js">IE7_PNG_SUFFIX=".png";</script>
+    <![endif]-->
 
+    = favicon_link_tag
+    /= render 'layouts/google_analytics'
 
   %body
-
-    %h1 Application Template
-
-
-    - [:error, :success, :notice].each do |type|
-      - if flash[type]
-        .type= flash[type]
-
-
-    = yield
+    #container
+      %header
+        - [:error, :success, :notice].each do |type|
+            - if flash[type]
+                .type= flash[type]
+      = yield
 END
-
-
 
 
 run "echo 'TODO' > README"
@@ -89,7 +73,7 @@ run "rm public/index.html"
 run 'bundle install'
 
 # Install haml/sass/compass 
-run "compass --rails -f blueprint . --css-dir=public/stylesheets/ --sass-dir=app/stylesheets"
+run "compass install blueprint/basic  --css-dir=public/stylesheets --sass-dir=app/stylesheets --images-dir=public/images -x sass"
 
 git :add => "."
 git :commit => "-a -m 'Finishing application setup'"
